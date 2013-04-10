77b279d78dcfb7cc237bb540ede20a0cc4cdffd8
Andy Biar <andybiar@gmail.com>
2013-04-10 02:35:09 -0400

    reformatted repo. fixed movement. added the first gate. textured sidewalks.

---
+++ up_UnityFolder/Assets/Standard Assets/HouseDriver.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+
+public class HouseDriver : MonoBehaviour {
+	// SET THESE IN THE INSPECTOR
+	public float lr_speed;
+	public float forward_speed;
+	public float up_speed;
+	public float max_height;
+	
+	private float base_speed;
+    private Vector3 moveDirection = Vector3.zero;
+	public CharacterController controller;
+	
+	private float xmov;
+
+	// Use this for initialization
+	void Start () {
+		base_speed = forward_speed;
+	}
+ 
+	// Update is called once per frame
+	void Update () {
+		xmov = 0;
+		
+		// Change the forward speed
+		if (Input.GetKey (KeyCode.Alpha1)) {
+			forward_speed = base_speed;
+		}
+		if (Input.GetKey (KeyCode.Alpha2)) {
+			forward_speed = base_speed * 2.0f;
+		}
+		if (Input.GetKey (KeyCode.Alpha3)) {
+			forward_speed = base_speed * 2.6f;
+		}
+		
+		///////// X Axis - Controlled by user input
+		if ( Input.GetKey (KeyCode.D) ) {
+			xmov += lr_speed * Time.deltaTime;
+		}
+		if ( Input.GetKey (KeyCode.A) ) {
+			xmov -= lr_speed * Time.deltaTime;
+		}
+		
+		//////// Built In (up/down balloon movement) Z on the jank
+		if (transform.position.y < max_height) {
+			transform.position += Vector3.up * up_speed * Time.deltaTime;
+		}
+		
+		//////// forward motion, affected by powerups/obstacles) -Y on the jank
+		transform.position += Vector3.forward * forward_speed * Time.deltaTime;
+		
+		Vector3 movdir = new Vector3(xmov, 0, 0);
+		controller.Move(movdir);
+	}
+}
---
+++ up_UnityFolder/Assets/otherScripts/DeltaGate.js
@@ -0,0 +1,19 @@
+#pragma strict
+
+public var target: GameObject;
+public var upspeedMultiplier: float = 1.0f;
+
+private var houseDriver: HouseDriver;
+
+private var passed: boolean = false;
+
+function Awake() {
+	houseDriver = target.GetComponent("HouseDriver");
+}
+
+function Update() {
+	if (!passed && target.transform.position.z > transform.position.z) {
+		houseDriver.up_speed *= upspeedMultiplier;
+		passed = true;
+	}
+}
